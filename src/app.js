16c342e50d5b60647db705432da2d6b43f28d035
Ian Brons <ianebrons@gmail.com>
2023-07-10 13:25:05 -0400

    added preview of new rectangle

--- src/app.js
+++ src/app.js
@@ -13,31 +13,31 @@ export class App {
         this.viewport = new Viewport({
             screenWidth: window.innerWidth,
             screenHeight: window.innerHeight,
-            // not sure what to set these values to
             worldWidth: 10000,
             worldHeight: 10000,
-
-            events: this.pixiApp.renderer.events // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+            events: this.pixiApp.renderer.events,
         });
 
         this.pixiApp.stage.addChild(this.viewport);
-        this.parts = null;
 
-        const partsTexture = PIXI.Assets.load('images/1.JPG');
-
+        let partsTexture = PIXI.Assets.load('images/1.JPG');
         partsTexture.then((texture) => {
             this.parts = new PIXI.Sprite(texture);
             this.parts.anchor.set(0, 0);
             this.viewport.addChild(this.parts);
             this.viewport.worldWidth = texture.width;
             this.viewport.worldHeight = texture.height;
-
             this.rectPlacement();
+
+            this.newRect = new PIXI.Graphics();
+            this.newRect.lineStyle(2, 0xFF0000, .75);
+            this.viewport.addChild(this.newRect);
         });
 
         this.crosshairs = new PIXI.Graphics();
         this.crosshairs.lineStyle(6, 0x808080, .4);
         this.pixiApp.stage.addChild(this.crosshairs);
+
 
         this.viewport.on('mousemove', (e) => {
             if (this.mode == 'edit') {
@@ -48,15 +48,17 @@ export class App {
                 this.crosshairs.lineTo(e.global.x, window.innerHeight);
                 this.crosshairs.moveTo(0, e.global.y);
                 this.crosshairs.lineTo(window.innerWidth, e.global.y);
+
+                if (this.firstPoint != null) {
+                    this.newRect.clear();
+                    let r = this.getRect(this.firstPoint, this.viewport.toWorld(e.clientX, e.clientY));
+                    this.newRect.lineStyle(2, 0xFF0000, .75);
+                    this.newRect.drawRect(r.x, r.y, r.width, r.height);
+                }
             }
         });
 
-        this.viewport
-            .drag()
-            .pinch()
-            .wheel()
-            .decelerate()
-            .clamp({ direction: 'all' });
+        this.viewport.drag().pinch().wheel().decelerate().clamp({ direction: 'all' });
 
         this.enterEditMode();
     }
@@ -80,19 +82,25 @@ export class App {
                 }
                 else {
                     let newPoint = this.viewport.toWorld(e.clientX, e.clientY);
-                    if (newPoint.x < this.firstPoint.x && newPoint.y < this.firstPoint.y) {
-                        let temp = { x: newPoint.x, y: newPoint.y }
-                        newPoint = this.firstPoint
-                        this.firstPoint = temp
-                    }
-                    let rect = new PIXI.Graphics();
-                    rect.lineStyle(5, 0xFF0000);
-                    rect.drawRect(this.firstPoint.x, this.firstPoint.y, newPoint.x - this.firstPoint.x, newPoint.y - this.firstPoint.y)
-                    this.viewport.addChild(rect);
+                    this.addPartRect(this.firstPoint, newPoint);
                     this.firstPoint = null;
                 }
             }
         });
+    }
+    getRect(p1, p2) {
+        let tl = { x: Math.min(p1.x, p2.x), y: Math.min(p1.y, p2.y) }
+        let br = { x: Math.max(p1.x, p2.x), y: Math.max(p1.y, p2.y) }
+        let width = br.x - tl.x
+        let height = br.y - tl.y;
+        return new PIXI.Rectangle(tl.x, tl.y, width, height);
+    }
+    addPartRect(p1, p2) {
+        let r = this.getRect(p1, p2);
+        let rect = new PIXI.Graphics();
+        rect.lineStyle(2, 0xFF0000, .75);
+        rect.drawRect(r.x, r.y, r.width, r.height);
+        this.viewport.addChild(rect);
     }
     enterSelectionMode() {
         this.mode = 'selection';
